9fcc0e5d5f8af74492575b48185c6cb3c6ac6f82
Kimie <kimie-tanaka@hotmail.com>
2020-10-13 22:14:55 -0300

    Corrige função

--- js/archive.js
+++ js/archive.js
@@ -1,20 +1,18 @@
-function insert(num){
-    debugger;
+function insert(num) {
     document.form.textview.value = document.form.textview.value+num;
 }
 
-function equal (){
+function equal() {
     var exp = document.form.textview.value;
     if(exp);
     document.form.textview.value = eval(exp);
 }
 
-function clean (){
-    debugger;
+function clean() {
     document.form.textview.value = "";
 }
 
-function back(){
-    var exp = document.form.textview.value;
-    document.form.textview.value = exp.substring(0,exp.lenght-1);
+function back() {
+    var aux = document.form.textview.value;
+    document.form.textview.value = aux.slice(0, -1);
 }
